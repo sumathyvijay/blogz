0f87f3f68c8b029975208918f61c081d942d73e5
Sumathy Dhakshinamurthy <sumathy.vijay@gmail.com>
2018-04-23 16:23:32 -0500

    Modified

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app=Flask(__name__)
 app.config['DEBUG']=True
-app.config['SQLALCHEMY_DATABASE_URI']= 'mysql+pymysql://blogz:launchcode@localhost:8889/blogz'
+app.config['SQLALCHEMY_DATABASE_URI']= 'mysql+pymysql://blogz:password@localhost:8889/blogz'
 app.config['SQLALCHEMY_ECHO']=True
 
 blogs=[]
